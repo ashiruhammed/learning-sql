1a1d3c1d8f4f40d9e8cc91f29d4017f82965ff36
ashiruhammed <hammedashiru89@gmail.com>
2025-05-03 15:25:56 +0100

    migrating a data fron a table intgo another

--- query_persons_table.sql
+++ query_persons_table.sql
@@ -1,6 +1,16 @@
 
-INSERT INTO persons(id, first_name, phone,EMAIL)
-VALUES (1, 'Ashiru', '09131200194', 'hammedashiru89@gmail.com')
+
+INSERT INTO persons
+SELECT 
+    id,
+    first_name,
+    NULL,
+    '09131200194' as phone,
+    'hammedashiru89@gmail.com'
+FROM customers
+WHERE id > 1
 
 
-SELECT * FROM persons
+
+
+
